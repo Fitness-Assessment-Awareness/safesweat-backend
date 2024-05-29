94caa8be37260622b4e2f29e414fae83dc0d4eff
KWT <weitengkoay@gmail.com>
2024-05-30 01:22:36 +0800

    Initial commit

---
+++ src/main/java/com/example/safesweatbackend/SafesweatBackendApplication.java
@@ -0,0 +1,15 @@
+package com.example.safesweatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
+
+@SpringBootApplication
+public class SafesweatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafesweatBackendApplication.class, args);
+    }
+
+}
